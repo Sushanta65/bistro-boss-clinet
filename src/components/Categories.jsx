3936e77e410d5e03744202c6183c4e064ccadef1
Sushanta65 <sushanta9119@gmail.com>
2025-01-09 00:29:56 +0600

    Menu API Fetch Done

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import SectionHeader from './SectionHeader';
+import React, { useEffect, useState } from "react";
+import SectionHeader from "./SectionHeader";
 
 const Categories = () => {
-    return (
-        <div>
-            <SectionHeader subHeader={'from 11:00PM to 10:00PM'} header={'Order Online'}></SectionHeader>
-        </div>
-    );
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((menuItems) => {
+        setMenu(menuItems);
+      });
+  }, []);
+  return (
+    <div className="w-4/5 mx-auto">
+      <SectionHeader
+        subHeader={"from 11:00PM to 10:00PM"}
+        header={"Order Online"}
+      ></SectionHeader>
+      <div>
+        {menu
+          .filter((item) => item.category === "popular")
+          .map((menuItem) => (
+            <div className="bg-gray-400 px-2 py-2 mt-5" key={menuItem.id}>
+              <h2>{menuItem.name}</h2>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
